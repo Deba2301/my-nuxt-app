035578724983f46a528c44e73ea24a41ac94b5db
Deba2301 <paridadebashish2301@gmail.com>
2025-09-13 10:26:19 +0530

    feat: changing app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen bg-transparent pt-16">
+    <div class="max-w-7xl mx-auto px-6 sm:px-8 lg:px-12">
+      <NuxtPage />
+    </div>
   </div>
 </template>
+
+
